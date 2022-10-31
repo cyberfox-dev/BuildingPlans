e763a6139cb2bca96c7b2c7d21cddd0c3a4f1f9d
cyberfox-dev <113167973+cyberfox-dev@users.noreply.github.com>
2022-10-31 14:50:47 +0200

    Added En information to array

--- ClientApp/src/app/new-user/new-egineer/new-egineer.component.ts
+++ ClientApp/src/app/new-user/new-egineer/new-egineer.component.ts
@@ -1,24 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
-export interface PeriodicElement {
+export interface EngineerList {
+  ProfessinalType: string;
+  professionalRegNo: string;
+  bpNumber: string;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-
-  cell: number;
+  surname: string;
   email: string;
-
+  phoneNumber: string;
+  
 }
 
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', cell: 526205658, email: 'jjjj@gmail.com' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', cell: 526205658, email: 'jjjj@gmail.com' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', cell: 526205658, email: 'jjjj@gmail.com' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', cell: 526205658, email: 'jjjj@gmail.com' },
-];
 
 @Component({
   selector: 'app-new-egineer',
@@ -27,14 +21,35 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class NewEgineerComponent implements OnInit {
   closeResult = '';
+
+  bpNoApplicant = '';
+  professionalRegNo = '';
+  name = '';
+  surname = '';
+  applicantTellNo = '';
+  applicantEmail = '';
+
+  @Input() tempEngineerList: EngineerList[] = [];
+
   constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
 
-  onAddEgineer() {
+  onAddEngineer() {
 
-    console.log("hit on click");
+    const newEnineer = {} as EngineerList;
+    newEnineer.ProfessinalType = "Engineer";
+    newEnineer.bpNumber = this.bpNoApplicant;
+    newEnineer.professionalRegNo = this.professionalRegNo;
+    newEnineer.name = this.name;
+    newEnineer.surname = this.surname;
+    newEnineer.email = this.applicantEmail;
+    newEnineer.phoneNumber = this.applicantTellNo;
+
+    this.tempEngineerList.push(newEnineer)
+
+    console.log("tempEngineerList:", this.tempEngineerList);
   }
 
   openXl(content: any) {
