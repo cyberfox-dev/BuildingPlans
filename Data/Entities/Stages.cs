b4b0bc1a1d2b9ccc25b371959cade5054f1638ee
BAD4SSDUCK <daksha.krishnan@gmail.com>
2022-11-01 15:04:05 +0200

    Stage key issue fix

--- Data/Entities/Stages.cs
+++ Data/Entities/Stages.cs
@@ -1,7 +1,10 @@
-﻿namespace WayleaveManagementSystem.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WayleaveManagementSystem.Data.Entities
 {
     public class Stages : BaseEntity
     {
+        [Key]
         public int? StageID { get; set; }
         public string? StageName { get; set; }
         public int? StageOrderNumber { get; set; }    
