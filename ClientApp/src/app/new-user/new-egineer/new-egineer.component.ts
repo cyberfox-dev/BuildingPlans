431c13f0f8876159e70a745f753fa42fb0162dc0
cyberfox-dev <113167973+cyberfox-dev@users.noreply.github.com>
2022-11-01 08:50:01 +0200

    New Commit Kyle

--- ClientApp/src/app/new-user/new-egineer/new-egineer.component.ts
+++ ClientApp/src/app/new-user/new-egineer/new-egineer.component.ts
@@ -11,24 +11,10 @@ export interface EngineerList {
   phoneNumber: string;
 
 }
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-
-  cell: number;
-  email: string;
-
-}
 
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', cell: 526205658, email: 'jjjj@gmail.com' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', cell: 526205658, email: 'jjjj@gmail.com' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', cell: 526205658, email: 'jjjj@gmail.com' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', cell: 526205658, email: 'jjjj@gmail.com' },
-];
+
+
 
 @Component({
   selector: 'app-new-egineer',
@@ -49,8 +35,8 @@ export class NewEgineerComponent implements OnInit {
 
   constructor(private modalService: NgbModal) { }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'cell', 'email', 'actions'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['Professinal Type', 'Professional Reg No', 'BP Number', 'Name', 'Surname', 'Email', 'Phone Number'];
+  dataSource = this.tempEngineerList;
 
   ngOnInit(): void {
   }
--- Data/Entities/Departments.cs
+++ Data/Entities/Departments.cs
@@ -12,9 +12,6 @@ namespace WayleaveManagementSystem.Data.Entities
         public DateTime DateCreated { get; set; }
         public DateTime DateUpdated { get; set; }
 
-        public int? CreatedById { get; set; }
-
-        public bool isActive { get; set; }
 
     }
 }
