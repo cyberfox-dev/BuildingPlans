7c78a88088dd7b0b9f4caf7281b8fc772f1861a4
Cyberfox <Cyberfox@DESKTOP-1B3OROO>
2022-11-01 08:30:18 +0200

    08-03 jahdiel

--- ClientApp/src/app/new-user/new-contractor/new-contractor.component.ts
+++ ClientApp/src/app/new-user/new-contractor/new-contractor.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+
+  cell: number;
+  email: string;
+
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', cell: 526205658, email: 'jjjj@gmail.com' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', cell: 526205658, email: 'jjjj@gmail.com' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', cell: 526205658, email: 'jjjj@gmail.com' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', cell: 526205658, email: 'jjjj@gmail.com' },
+];
 
 @Component({
   selector: 'app-new-contractor',
@@ -18,6 +36,8 @@ export class NewContractorComponent implements OnInit {
   ContractorEmail = '';
 
 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'cell', 'email', 'actions'];
+  dataSource = ELEMENT_DATA;
 
   constructor(private modalService: NgbModal) { }
 
