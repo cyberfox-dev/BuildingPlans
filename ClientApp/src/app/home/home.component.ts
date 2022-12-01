b1dcc9f766d54e6015e6fc02a3c103018b5c7574
cyberfox-dev <113167973+cyberfox-dev@users.noreply.github.com>
2022-12-01 14:13:13 +0200

    Home

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -7,12 +7,17 @@ import { Router, ActivatedRoute, Route, Routes } from "@angular/router";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
-
+  CurrentUser: any;
+  stringifiedData: any;
   constructor(private router: Router) {
 
   }
 
   ngOnInit(): void {
+    this.stringifiedData = JSON.parse(JSON.stringify(localStorage.getItem('LoggedInUserInfo')));
+    this.CurrentUser = JSON.parse(this.stringifiedData);
+
+
   }
   viewProject() {
     this.router.navigate(["/view-project-info"]);
