63bd879af1f86845f685af24c2388090e22bc251
Cyberfox <Cyberfox@DESKTOP-1B3OROO>
2022-12-02 15:13:37 +0200

    edits made to new wayleave

--- ClientApp/src/app/create-new-wayleave/new-wayleave/new-wayleave.component.ts
+++ ClientApp/src/app/create-new-wayleave/new-wayleave/new-wayleave.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ApplicationsService } from 'src/app/service/Applications/applications.service';
+/*import { ApplicationsService } from 'src/app/service/Applications/applications.service';*/
 import { FormBuilder, Validators } from '@angular/forms';
 import { empty } from 'rxjs';
 import { InternalOptionComponent } from 'src/app/create-new-wayleave/internal-option/internal-option.component';
@@ -11,23 +11,18 @@ import { SharedService } from "../../shared/shared.service";
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  bp: string;
+  surname: string;
+  professionalRegNumber: string;
+
+
 }
 
+
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { bp: 'fdf', name: 'FullName', surname: "", professionalRegNumber: 'H' },
 ];
+
 @Component({
   selector: 'app-new-wayleave',
   /*  templateUrl: './new-wayleave.component.html',*/
@@ -58,7 +53,7 @@ export class NewWayleaveComponent implements OnInit {
     this.CurrentUser = JSON.parse(this.stringifiedData); this.stringifiedData = JSON.parse(JSON.stringify(localStorage.getItem('LoggedInUserInfo')));
   }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['bp', 'name', 'surname', 'professionalRegNumber'];
   dataSource = ELEMENT_DATA;
   clickedRows = new Set<PeriodicElement>();
 
