62c692fbbe57838c8928991b105f5580e6c85da1
Cyberfox <Cyberfox@DESKTOP-1B3OROO>
2022-12-05 09:31:43 +0200

    document upload

--- ClientApp/src/app/view-project/view-project-info/view-project-info.component.ts
+++ ClientApp/src/app/view-project/view-project-info/view-project-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 export interface PeriodicElement {
   name: string;
@@ -13,6 +14,17 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
 ];
 
+export interface Documents {
+  name: string;
+
+}
+
+const Document_DATA: Documents[] = [
+  { name: 'doc1' },
+  { name: 'doc2'  },
+  { name: 'doc3' },
+];
+
 @Component({
   selector: 'app-view-project-info',
   templateUrl: './view-project-info.component.html',
@@ -20,11 +32,44 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ViewProjectInfoComponent implements OnInit {
 
+  @ViewChild('fileInput') fileInput: ElementRef | undefined;
+  fileAttr = 'Choose File';
+  uploadFileEvt(imgFile: any) {
+    if (imgFile.target.files && imgFile.target.files[0]) {
+      this.fileAttr = '';
+      Array.from(imgFile.target.files).forEach((file: any) => {
+        this.fileAttr += file.name + ' - ';
+      });
+      // HTML5 FileReader API
+      let reader = new FileReader();
+      reader.onload = (e: any) => {
+        let image = new Image();
+        image.src = e.target.result;
+        image.onload = (rs) => {
+          let imgBase64Path = e.target.result;
+        };
+      };
+      reader.readAsDataURL(imgFile.target.files[0]);
+      // Reset if duplicate image uploaded again
+
+    } else {
+      this.fileAttr = 'Choose File';
+    }
+  }
+
+  openDocUpload(newSub: any) {
+    this.modalService.open(newSub, { backdrop: 'static', centered: true, size: 'lg' });
+  }
+
+
   panelOpenState = false;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','actions'];
   dataSource = ELEMENT_DATA;
 
-  constructor() { }
+  displayedColumnsDocs: string[] = ['name','actions'];
+  dataSourceDoc = Document_DATA;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
