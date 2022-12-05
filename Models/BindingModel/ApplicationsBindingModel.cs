517a39732df92b6eb22d58bd93c1b0ece74b9de7
venolin <venolin1@gmail.com>
2022-12-05 08:53:05 +0200

    Changes to application capture process.

--- Models/BindingModel/ApplicationsBindingModel.cs
+++ Models/BindingModel/ApplicationsBindingModel.cs
@@ -4,7 +4,7 @@
     {
         public int? ApplicationID { get; set; }
         public string? UserID { get; set; }
-        public string FullName { get; set; }
+        public string? FullName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? PhysicalAddress { get; set; }
