42a04943be0d342627e5a47dbcc43dc4fac860f8
Cyberfox <Cyberfox@DESKTOP-1B3OROO>
2023-02-15 11:39:45 +0200

    service items

--- ClientApp/src/app/service-items-config/service-items-config.component.ts
+++ ClientApp/src/app/service-items-config/service-items-config.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+
+export interface ServiceItemList {
+  serviceItemID: number;
+  serviceItemCode: string;
+  Description: string;
+  Rate: any;
+  totalVat: number;
+}
 
 @Component({
   selector: 'app-service-items-config',
   templateUrl: './service-items-config.component.html',
   styleUrls: ['./service-items-config.component.css']
 })
+
+
+
 export class ServiceItemsConfigComponent implements OnInit {
 
-  constructor() { }
+  ServiceItemList: ServiceItemList[] = [];
+
+  CurrentUser: any;
+  stringifiedData: any;
+  constructor(private modalService: NgbModal,) { }
+
+  serviceItemCode = '';
+  description = '';
+  rate = '';
+  quantity = '';
+  total = '';
+  checked: boolean = false;
+
+
 
   ngOnInit(): void {
+
+    this.stringifiedData = JSON.parse(JSON.stringify(localStorage.getItem('LoggedInUserInfo')));
+    this.CurrentUser = JSON.parse(this.stringifiedData); this.stringifiedData = JSON.parse(JSON.stringify(localStorage.getItem('LoggedInUserInfo')));
+    this.CurrentUser = JSON.parse(this.stringifiedData);
+  }
+
+  @ViewChild(MatTable) ServiceItemTable: MatTable<ServiceItemList> | undefined;
+
+  displayedColumnsServiceItems: string[] = ['serviceItemCode', 'dateCreated', 'actions'];
+  dataSourceServiceItems = this.ServiceItemList;
+
+
+  createNewServiceModal(addServiceItem: any) {
+    this.modalService.open(addServiceItem, { size: 'xl' });
   }
 
 }
