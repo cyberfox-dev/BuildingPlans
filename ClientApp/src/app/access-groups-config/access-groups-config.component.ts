51efaa35182042f98e124ac174d23e73c28fa1df
cyberfox-dev <113167973+cyberfox-dev@users.noreply.github.com>
2023-04-18 09:19:45 +0200

    More

--- ClientApp/src/app/access-groups-config/access-groups-config.component.ts
+++ ClientApp/src/app/access-groups-config/access-groups-config.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { AccessGroupsService } from 'src/app/service/AccessGroups/access-groups.service';
+import { UntypedFormGroup, FormBuilder, Validators } from '@angular/forms';
 
 export interface PeriodicElement {
   name: string;
-
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
@@ -21,6 +23,16 @@ const LinkUsersToZone: PeriodicElement[] = [
 ];
 
 
+export interface AccessGroupList {
+  AccessGroupID: number,
+  AccessGroupDescription: string,
+  AccessGroupName: string,
+  //CreatedById: string,
+  DateCreated: string,
+  DateUpdated: string,
+}
+
+
 @Component({
   selector: 'app-access-groups-config',
   templateUrl: './access-groups-config.component.html',
@@ -31,21 +43,102 @@ const LinkUsersToZone: PeriodicElement[] = [
 
 export class AccessGroupsConfigComponent implements OnInit {
 
+
+  AccessGroupList: AccessGroupList[] = [];
+
+  public addAccessGroup = this.formBuilder.group({
+    accessGroupName: ['', Validators.required],
+    accessGroupDescription: ['', Validators.required]
+
+  })
+    stringifiedData: any;
+    CurrentUser: any;
+
   openXl(content: any) {
     this.modalService.open(content, { size: 'lg' });
   }
   openAddUserToAccessGroup(addUserToAccessGroup :any) {
     this.modalService.open(addUserToAccessGroup, { centered:true,size: 'lg' });
   }
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private accessGroupsService: AccessGroupsService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.getAllAccessGroup();
+    this.stringifiedData = JSON.parse(JSON.stringify(localStorage.getItem('LoggedInUserInfo')));
+    this.CurrentUser = JSON.parse(this.stringifiedData);
+    
   }
 
-    displayedColumns: string[] = ['name', 'actions'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['AccessGroupName','AccessGroupDescription' ,'actions'];
+  dataSource = this.AccessGroupList;
 
   displayedColumnsAddUser: string[] = ['name', 'actions'];
   dataSourceAddUser = LinkUsersToZone;
+
+  @ViewChild(MatTable) AccessGroupListTable: MatTable<AccessGroupList> | undefined;
+
+
+  onAccessGroupCreate() {
+    let accessGroupName = this.addAccessGroup.controls["accessGroupName"].value;
+    let accessGroupDescription = this.addAccessGroup.controls["accessGroupDescription"].value;
+
+    this.accessGroupsService.addUpdateAccessGroup(0, accessGroupDescription, accessGroupName,this.CurrentUser.appUserId).subscribe((data: any) => {
+
+      if (data.responseCode == 1) {
+
+        alert(data.responseMessage);
+        this.getAllAccessGroup();
+        this.addAccessGroup.controls["accessGroupName"].setValue("");
+        this.addAccessGroup.controls["accessGroupDescription"].setValue("");
+
+      }
+      else {
+
+        alert(data.responseMessage);
+      }
+      console.log("reponse", data);
+
+    }, error => {
+      console.log("Error: ", error);
+    })
+
+  }
+
+ 
+  getAllAccessGroup() {
+    this.AccessGroupList.splice(0, this.AccessGroupList.length);
+    this.accessGroupsService.getAllAccessGroups().subscribe((data: any) => {
+
+      if (data.responseCode == 1) {
+
+
+        for (let i = 0; i < data.dateSet.length; i++) {
+          const tempAccessGroupList = {} as AccessGroupList;
+          const current = data.dateSet[i];
+          tempAccessGroupList.AccessGroupID = current.accessGroupID;
+          tempAccessGroupList.AccessGroupDescription = current.accessGroupDescription;
+          tempAccessGroupList.AccessGroupName = current.accessGroupName;
+          tempAccessGroupList.DateUpdated = current.dateUpdated;
+          tempAccessGroupList.DateCreated = current.dateCreated;
+          this.AccessGroupList.push(tempAccessGroupList);
+
+        }
+        this.AccessGroupListTable?.renderRows();
+     
+
+
+        console.log("AccessGroupList", this.AccessGroupList);
+      }
+      else {
+
+        alert(data.responseMessage);
+      }
+      console.log("reponse", data);
+
+    }, error => {
+      console.log("Error: ", error);
+    })
+  }
+
 
 }
---
+++ Data/Migrations/20230417071051_AccessGroups.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WayleaveManagementSystem.Data.Migrations
+{
+    public partial class AccessGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccessGroupRoleLink",
+                columns: table => new
+                {
+                    AccessGroupRoleLinkID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccessGroupID = table.Column<int>(type: "int", nullable: true),
+                    RoleID = table.Column<int>(type: "int", nullable: true),
+                    RoleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedById = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessGroupRoleLink", x => x.AccessGroupRoleLinkID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccessGroups",
+                columns: table => new
+                {
+                    AccessGroupID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccessGroupName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccessGroupDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedById = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessGroups", x => x.AccessGroupID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccessGroupUserLink",
+                columns: table => new
+                {
+                    AccessGroupUserLinkID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccessGroupID = table.Column<int>(type: "int", nullable: true),
+                    UserID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedById = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessGroupUserLink", x => x.AccessGroupUserLinkID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccessGroupRoleLink");
+
+            migrationBuilder.DropTable(
+                name: "AccessGroups");
+
+            migrationBuilder.DropTable(
+                name: "AccessGroupUserLink");
+        }
+    }
+}
