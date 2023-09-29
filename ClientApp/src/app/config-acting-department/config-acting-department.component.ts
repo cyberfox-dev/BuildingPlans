61facace402c6b5c1a73976ddf349eebcf283dfe
cyberfox-dev <113167973+cyberfox-dev@users.noreply.github.com>
2023-09-29 09:35:48 +0200

    Config

--- ClientApp/src/app/config-acting-department/config-acting-department.component.ts
+++ ClientApp/src/app/config-acting-department/config-acting-department.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-config-acting-department',
   templateUrl: './config-acting-department.component.html',
   styleUrls: ['./config-acting-department.component.css']
 })
+
+
+
 export class ConfigActingDepartmentComponent implements OnInit {
+  @Input() isInternal: any;
+
+
 
   constructor() { }
 
