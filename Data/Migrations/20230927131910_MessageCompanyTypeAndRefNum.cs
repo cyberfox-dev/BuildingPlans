0c0698ce185233d8ffca7aac4853273c48358eca
cyberfox-dev <113167973+cyberfox-dev@users.noreply.github.com>
2023-10-05 16:19:53 +0200

    Mi

--- Data/Migrations/20230927131910_MessageCompanyTypeAndRefNum.cs
+++ Data/Migrations/20230927131910_MessageCompanyTypeAndRefNum.cs
@@ -8,38 +8,38 @@ namespace WayleaveManagementSystem.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "Notification",
+                type: "nvarchar(max)",
+                nullable: true);
+
             //        migrationBuilder.AddColumn<string>(
-            //            name: "Message",
-            //            table: "Notification",
+            //         name: "companyType",
+            //table: "UserProfilesTable",
+            //type: "nvarchar(max)",
+            //nullable: true);
+
+            //        migrationBuilder.AddColumn<string>(
+            //            name: "refNumber",
+            //            table: "UserProfilesTable",
             //            type: "nvarchar(max)",
             //            nullable: true);
-
-    //        migrationBuilder.AddColumn<string>(
-    //         name: "companyType",
-    //table: "UserProfilesTable",
-    //type: "nvarchar(max)",
-    //nullable: true);
-
-    //        migrationBuilder.AddColumn<string>(
-    //            name: "refNumber",
-    //            table: "UserProfilesTable",
-    //            type: "nvarchar(max)",
-    //            nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "Notification");
+
             //        migrationBuilder.DropColumn(
-            //            name: "Message",
-            //            table: "Notification");
+            //         name: "companyType",
+            //table: "UserProfilesTable");
 
-    //        migrationBuilder.DropColumn(
-    //         name: "companyType",
-    //table: "UserProfilesTable");
-
-    //        migrationBuilder.DropColumn(
-    //            name: "refNumber",
-    //            table: "UserProfilesTable");
+            //        migrationBuilder.DropColumn(
+            //            name: "refNumber",
+            //            table: "UserProfilesTable");
         }
     }
 }
---
+++ Data/Migrations/20231004075013_mess.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WayleaveManagementSystem.Data.Migrations
+{
+    public partial class mess : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+      name: "Message",
+      table: "Notification",
+      type: "nvarchar(max)",
+      nullable: true);
+            migrationBuilder.AddColumn<string>(
+           name: "DescriptionForRepoDoc",
+           table: "DocumentUpload",
+           type: "nvarchar(max)",
+           nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+               name: "Message",
+               table: "Notification"); migrationBuilder.DropColumn(
+                name: "Message",
+                table: "Notification");
+            migrationBuilder.DropColumn(
+               name: "DescriptionForRepoDoc",
+               table: "DocumentUpload");
+        }
+
+    }
+}
