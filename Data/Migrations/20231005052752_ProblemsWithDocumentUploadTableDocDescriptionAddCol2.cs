b98b7d34273bd3f672c9323a112b0782d91427d7
cyberfox-dev <113167973+cyberfox-dev@users.noreply.github.com>
2023-10-06 08:35:21 +0200

    Mi change

--- Data/Migrations/20231005052752_ProblemsWithDocumentUploadTableDocDescriptionAddCol2.cs
+++ Data/Migrations/20231005052752_ProblemsWithDocumentUploadTableDocDescriptionAddCol2.cs
@@ -8,19 +8,19 @@ namespace WayleaveManagementSystem.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-             name: "DescriptionForRepoDoc",
-             table: "DocumentUpload",
-             type: "nvarchar(max)",
-             nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            // name: "DescriptionForRepoDoc",
+            // table: "DocumentUpload",
+            // type: "nvarchar(max)",
+            // nullable: true);
 
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-             name: "DescriptionForRepoDoc",
-              table: "DocumentUpload");
+            //migrationBuilder.DropColumn(
+            // name: "DescriptionForRepoDoc",
+            //  table: "DocumentUpload");
         }
     }
 }
