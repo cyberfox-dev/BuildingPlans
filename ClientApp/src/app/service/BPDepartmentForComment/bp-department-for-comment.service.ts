8e97c84e07fd282c5898b57aef10fce5e9b72758
Cyberfox <jahdiel@cyberfox.co.za>
2024-07-25 11:54:45 +0200

    JJS Commit

---
+++ ClientApp/src/app/service/BPDepartmentForComment/bp-department-for-comment.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SharedService } from 'src/app/shared/shared.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BpDepartmentForCommentService {
+
+  private readonly apiUrl: string = this.sharedService.getApiUrl() + '/api/';
+  private readonly baseURL: string = this.apiUrl + "BpDepartmentForCommentService/";
+  constructor(private httpClient: HttpClient, private sharedService: SharedService) { }
+
+  public addUpdateDepartmentForComment(BPDepartmentForCommentID: number | null, applicationID: number | null, DepartmentID: number | null, DepartmentName: string | null, userAssaignedToComment: string | null, commentStatus: string | null, createdByID: string | null) {
+
+    const body = {
+      BPDepartmentForCommentID: BPDepartmentForCommentID,
+      ApplicationID: applicationID,
+      DepartmentID: DepartmentID,
+      DepartmentName: DepartmentName,
+      UserAssaignedToComment: userAssaignedToComment,
+      CommentStatus: commentStatus,
+      CreatedById: createdByID,
+
+    }
+    return this.httpClient.post(this.baseURL + "AddUpdateDepartmentForComment", body);
+
+  }
+
+  public departmentForCommentFinalAppovalUserToComment(subDepartmentForCommentID: number | null, userAssaignedToComment: string | null) {
+
+    const body = {
+      SubDepartmentForCommentID: subDepartmentForCommentID,
+      UserAssaignedToComment: userAssaignedToComment,
+
+    }
+    return this.httpClient.post(this.baseURL + "DepartmentForCommentFinalAppovalUserToComment", body);
+
+  }
+
+  public deleteDepartmentForComment(subDepartmentForCommentID: number) {
+
+    return this.httpClient.post(this.baseURL + "DeleteDepartmentForComment", subDepartmentForCommentID);
+
+  }
+
+
+
+  public getDepartmentForComment(applicationID: number | null) {
+
+    const body = {
+      ApplicationID: applicationID,
+    }
+    return this.httpClient.post(this.baseURL + "GetDepartmentForComment", body);
+
+  }
+  public getSubDepartmentForCommentBySubID(applicationID: number | null, DepartmentID: number | null, userID?: number | null) {
+
+    const body = {
+      ApplicationID: applicationID,
+      DepartmentID: DepartmentID,
+      UserAssaignedToComment: userID,
+    }
+    return this.httpClient.post(this.baseURL + "GetDepartmentForCommentBySubID", body);
+
+  }
+
+
+
+}
